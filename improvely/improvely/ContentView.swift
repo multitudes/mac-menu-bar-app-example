c357b28ed5b6cbb11a6acd88692fc9c10a6a9e8b
Laurent Brusa <wrmultitudes@gmail.com>
2023-08-11 13:31:39 +0200

    start of project

---
+++ improvely/improvely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  improvely
+//
+//  Created by Laurent B on 11/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("improvely")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ improvely/improvely/improvelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  improvelyApp.swift
+//  improvely
+//
+//  Created by Laurent B on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct improvelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
