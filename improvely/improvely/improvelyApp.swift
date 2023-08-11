f0f81c6bb073a5e3556d3abfb17d5d5705583bf9
Laurent Brusa <wrmultitudes@gmail.com>
2023-08-11 15:02:53 +0200

    small app ready to show menu

--- improvely/improvely/improvelyApp.swift
+++ improvely/improvely/improvelyApp.swift
@@ -9,9 +9,34 @@ import SwiftUI
 
 @main
 struct improvelyApp: App {
+    @AppStorage("showMenuBarExtra") private var showMenuBarExtra = true
+    @State private var symbol: String = "figure.run.square.stack"
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        //        WindowGroup {
+        //            ContentView()
+        //        }
+        MenuBarExtra(symbol, systemImage: symbol) {
+            Button("Improve!") {
+                symbol = "mountain.2.fill"
+            }
+            .keyboardShortcut("k")
+            
+            Button("Two") {
+                symbol = "figure.run.square.stack.fill"
+            }
+            .keyboardShortcut("g")
+            
+            Button("Three") {
+                symbol = "sos"
+            }
+            .keyboardShortcut("1")
+            
+            Divider()
+            
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
         }
     }
 }
