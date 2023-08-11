57fddcf5ab544221a59de6f024085f34505bcd8e
Laurent Brusa <wrmultitudes@gmail.com>
2023-08-11 15:13:54 +0200

    Update improvelyApp.swift

--- improvely/improvely/improvelyApp.swift
+++ improvely/improvely/improvelyApp.swift
@@ -11,12 +11,26 @@ import SwiftUI
 struct improvelyApp: App {
     @AppStorage("showMenuBarExtra") private var showMenuBarExtra = true
     @State private var symbol: String = "figure.run.square.stack"
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some Scene {
         //        WindowGroup {
         //            ContentView()
         //        }
         MenuBarExtra(symbol, systemImage: symbol) {
+            // just for fun
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(0x1f600...0x1f679, id: \.self) { value in
+                        Text(String(format: "%x", value))
+                        Text(emoji(value))
+                            .font(.largeTitle)
+                    }
+                }
+            }
+            
+            
+            
             Button("Improve!") {
                 symbol = "mountain.2.fill"
             }
@@ -38,5 +52,11 @@ struct improvelyApp: App {
                 NSApplication.shared.terminate(nil)
             }.keyboardShortcut("q")
         }
+        .menuBarExtraStyle(.window)
+    }
+    
+    private func emoji(_ value: Int) -> String {
+        guard let scalar = UnicodeScalar(value) else { return "?" }
+        return String(Character(scalar))
     }
 }
